0428d70558e4e01c3f69dbe6a41e94bc9690ccdb
kaylawoodbury <kaylasandoval92@gmail.com>
2020-01-27 16:08:04 +0100

    adding additional test for submit button and that name and number of new contact are displayed

--- cypress/integration/integration.userCanCreateAContact.feature.js
+++ cypress/integration/integration.userCanCreateAContact.feature.js
@@ -2,6 +2,7 @@
 1. User can access homepage (the localhost:3001)
 2. User can click on an element called 'add-contact'
 3. User can add contact elements, name number etc
+4. User can click save element
 */
 
 describe('user can create a contact', () => {
@@ -14,5 +15,14 @@ describe('user can create a contact', () => {
         cy.get('#company').type('Craft Academy')
         cy.get('#notes').type('Learning to code')
         cy.get('#twitter').type('@kayla')
+        cy.get('#submit').click()
+    })
+
+    it('displays the name of the new contact', () => {
+        cy.get('#contact-list').should('contain', 'Kayla')
+    })
+
+    it('displays the phone number of the new contact', () => {
+        cy.get('#contact-list').should('contain', '0700 101010')
     })
 })
