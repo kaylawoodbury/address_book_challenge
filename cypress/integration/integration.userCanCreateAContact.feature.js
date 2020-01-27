d7f065caf649c703410e16a83affefc634392210
kaylawoodbury <kaylasandoval92@gmail.com>
2020-01-27 12:41:06 +0100

    adding test for new contact information and adding new form in html to get user input for contact info

--- cypress/integration/integration.userCanCreateAContact.feature.js
+++ cypress/integration/integration.userCanCreateAContact.feature.js
@@ -1,11 +1,18 @@
-/* first tests for address book
+/* add contact tests for address book
 1. User can access homepage (the localhost:3001)
 2. User can click on an element called 'add-contact'
+3. User can add contact elements, name number etc
 */
 
 describe('user can create a contact', () => {
     it('test', () => {
         cy.visit('http://localhost:3001')
         cy.get('#add-contact').click()
+        cy.get('#name').type('Kayla')
+        cy.get('#email').type('kaylasandoval@gmail.com')
+        cy.get('#phone').type('0700 101010')
+        cy.get('#company').type('Craft Academy')
+        cy.get('#notes').type('Learning to code')
+        cy.get('#twitter').type('@kayla')
     })
 })
