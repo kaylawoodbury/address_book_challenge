06a9b66cf2e845ae66584debbd2f3a3026056b87
kaylawoodbury <kaylasandoval92@gmail.com>
2020-01-27 11:25:18 +0100

    creating first test .js file to test that user can access homepage

---
+++ cypress/integration/integration.userCanCreateAContact.feature.js
@@ -0,0 +1,9 @@
+/* first tests for address book
+1. User can access homepage (the localhost:3001)
+*/
+
+describe('user can create a contact', () => {
+    it('test', () => {
+        cy.visit('http://localhost:3001')
+    })
+})
